46862ffa98ba5aae8d9f605035afabf9179c21d1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-11-28 12:35:02 +0100

    Configuración para que arranque Elastic en el M1

--- sonarqube-9-community/sonar.sh
+++ sonarqube-9-community/sonar.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+# https://newbedev.com/how-to-execute-scripts-after-docker-compose-up
+if [ ! -f .initialized ]; then
+  echo "bootstrap.seccomp: false" >>/opt/sonarqube/elasticsearch/config/elasticsearch.yml
+  echo "bootstrap.system_call_filter: false" >>/opt/sonarqube/elasticsearch/config/elasticsearch.yml
+  touch .initialized
+fi
+
 exec java -jar lib/sonar-application-"${SONAR_VERSION}".jar -Dsonar.log.console=true "$@"
