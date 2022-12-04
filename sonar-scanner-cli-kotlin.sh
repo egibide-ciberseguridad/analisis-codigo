556e731fe00dcee21ccd1de7a746647385932f95
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-12-04 12:08:27 +0100

    Scripts para Linux y macOS Intel

---
+++ sonar-scanner-cli-kotlin.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+if [ $# -lt 3 ]; then
+  echo ''
+  echo 'Uso:  sonar-scanner-cli-kotlin.sh TOKEN RUTA_PROYECTO NOMBRE_PROYECTO'
+  exit 1
+fi
+
+docker run \
+  --rm \
+  --network ciberseguridad_sonarnet \
+  -e SONAR_HOST_URL="http://sonarqube:9000" \
+  -e SONAR_LOGIN="$1" \
+  -v "$2:/usr/src" \
+  sonarsource/sonar-scanner-cli \
+  -D sonar.exclusions='**/*.java' \
+  -D sonar.projectKey="$3"
---
+++ sonar-scanner-cli-maven.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+if [ $# -lt 3 ]; then
+  echo ''
+  echo 'Uso:  sonar-scanner-cli-maven.sh TOKEN RUTA_PROYECTO NOMBRE_PROYECTO'
+  exit 1
+fi
+
+docker run \
+  --rm \
+  --network ciberseguridad_sonarnet \
+  -e SONAR_HOST_URL="http://sonarqube:9000" \
+  -e SONAR_LOGIN="$1" \
+  -v "$2:/usr/src" \
+  sonarsource/sonar-scanner-cli \
+  -D sonar.java.binaries=./target/classes/ \
+  -D sonar.projectKey="$3"
