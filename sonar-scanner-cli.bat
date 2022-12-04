6e650b601060555458945f43cacbfb421d49ad24
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-12-04 18:30:31 +0100

    Scanner para Windows

---
+++ sonar-scanner-cli.bat
@@ -0,0 +1,22 @@
+@echo off
+
+if "%~1" == "" goto error
+if "%~2" == "" goto error
+if "%~3" == "" goto error
+
+docker run ^
+  --rm ^
+  --network ciberseguridad_sonarnet ^
+  -e SONAR_HOST_URL="http://sonarqube:9000" ^
+  -e SONAR_LOGIN="%1" ^
+  -v "%2:/usr/src" ^
+  sonarsource/sonar-scanner-cli ^
+  -D sonar.projectKey="%3"
+
+exit /b 0
+
+:error
+  echo.
+  echo Uso:  sonar-scanner-cli.bat TOKEN RUTA_PROYECTO NOMBRE_PROYECTO
+  echo.
+  exit /b 1
